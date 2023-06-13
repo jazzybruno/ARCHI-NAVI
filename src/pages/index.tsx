7217729a88672ed212da5faf683b5b501bfdd44e
Tenma Endou <pepci.oishii@gmail.com>
2023-06-13 15:27:59 +0900

    use swr

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,18 +2,26 @@ import type { GetServerSideProps, NextPageWithLayout } from 'next'
 import Link from 'next/link'
 import { format } from 'date-fns'
 import { MainLayout } from 'src/layouts/main'
-import { httpClient } from 'src/services/httpClient'
+import { fetcher, httpClient } from 'src/services/httpClient'
+import useSWR from "swr"
+import { User } from 'src/types'
+import { useEffect, useState } from 'react'
 
 type Props = {
   title?: string;
 };
 
 const HomePage: NextPageWithLayout<Props> = (props) => {
+  const [isClient, setIsClient] = useState<boolean>(false);
+  useEffect(() => setIsClient(true), []);
+  const { data: user, error } = useSWR("api/user", fetcher<User>)
+  const date = format(new Date(), 'yyyy-MM-dd HH:mm:ss')
   return (
     <>
       <h1>{ props.title }</h1>
+      <h3>{ user?.name ?? '未ログイン' }</h3>
       <p>
-        { format(new Date(), 'yyyy-MM-dd') }
+        { isClient ? date : '' }
       </p>
       <p>
         <Link href="/about">About</Link>
@@ -23,7 +31,7 @@ const HomePage: NextPageWithLayout<Props> = (props) => {
 }
 
 export const getServerSideProps: GetServerSideProps = async () => {
-  const res = await httpClient().get('http://127.0.0.1:8000/api/test')
+  const res = await httpClient().get('api/test')
   const props: Props = {
     title: res.data,
   }
--- src/services/httpClient.ts
+++ src/services/httpClient.ts
@@ -2,12 +2,17 @@ import axios from "axios"
 
 const getAuthHeader = () => {
   return {
+    Authorization: `Bearer `,
   }
 }
 
 axios.defaults.withCredentials = true
 export const httpClient = () => axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL || 'http://127.0.0.1:8000',
+  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL || 'http://localhost:8000',
   headers: getAuthHeader(),
-  withCredentials: true
+  withCredentials: true,
 })
+
+export async function fetcher<T>(key: string, init?: RequestInit) {
+  return httpClient().get(key).then((res) => res.data as Promise<T | null>);
+}
