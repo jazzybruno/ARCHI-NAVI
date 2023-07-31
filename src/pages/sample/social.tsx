4f2ab99f95e15ba9c8d9180d7afe86c7e4dfa277
Tenma Endou <pepci.oishii@gmail.com>
2023-07-31 12:53:04 +0900

    redirect url

--- src/pages/sample/social.tsx
+++ src/pages/sample/social.tsx
@@ -6,7 +6,7 @@ const SocialPage: NextPageWithLayout = () => {
   const router = useRouter()
 
   async function signin(provider: string) {
-    router.push(`${process.env.NEXT_PUBLIC_API_BASE_URL}/auth/redirect/${provider}`)
+    router.push(`${process.env.NEXT_PUBLIC_API_BASE_URL}/auth/redirect/${provider}?redirect_url=${process.env.NEXT_PUBLIC_APP_URL}/mypage`)
   }
 
   return (
